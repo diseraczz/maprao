4bc3b98ac4599599ddec0efb4435b2e4a7c3b821
diseraczz <pkpeeradol@gmail.com>
2025-09-23 01:10:42 +1000

    first Nest.js comit

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/diseraczz.github.io',
+  assetPrefix: '/diseraczz.github.io/',
+};
+
+export default nextConfig;
